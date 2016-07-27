97427d47b87ca7b17858504702e87f5b6c748985
Filippe <revigat@gmail.com>
2016-07-27 16:39:29 -0300

    Ajuste administradores

--- breadtimesite/views.py
+++ breadtimesite/views.py
@@ -1,6 +1,8 @@
+# -*- coding: UTF-8 -*-
 from django.shortcuts import render, HttpResponse
 from django.core import serializers
 from django.shortcuts import render_to_response
+from django.core.serializers.json import DjangoJSONEncoder
 from breadtimesite.models import *
 
 # Create your views here.
@@ -13,4 +15,4 @@ def index(request):
 def exportarfeed(request):
 	data = serializers.serialize('json', list(Post.objects.all()),indent=2,use_natural_foreign_keys=True, use_natural_primary_keys=True)
 	#Habilita o nome legivel 
-	return HttpResponse(data, content_type="application/json")
+	return HttpResponse(data,content_type="application/json; charset=utf-8")
