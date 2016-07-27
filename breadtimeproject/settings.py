72078424f80e8c81779b6b90f9bd3917c6e850da
Filippe <revigat@gmail.com>
2016-07-27 05:25:15 -0300

    Ajuste para deploy no Heroku

--- breadtimeproject/settings.py
+++ breadtimeproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'xu802sl1&-=%4!g65=7ez^10lc!sq4w(_x^=&l(0n(s6pqfcq_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://breadtime.herokuapp.com']
 
 
 # Application definition
