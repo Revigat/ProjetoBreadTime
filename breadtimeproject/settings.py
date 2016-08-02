12d2817e7079032b4f32eac4391dcf5dcb29389a
filippe <revigat@gmail.com>
2016-08-02 03:57:04 -0300

    Commit para teste de json

--- breadtimeproject/settings.py
+++ breadtimeproject/settings.py
@@ -91,8 +91,7 @@ DATABASES = {
         'PORT': '', #8000 DEFAULT
     }
 }
-DATABASES['default'] = dj_database_url.parse('postgres://@Pro502010:127.0.0.1:8000/breadtime.db')
-postgres://USER:PASSWORD@HOST:PORT/NAME
+DATABASES['breadtime.db'] = dj_database_url.parse('postgres://postgres:@Pro502010@127.0.0.1:8000/breadtime.db')
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
