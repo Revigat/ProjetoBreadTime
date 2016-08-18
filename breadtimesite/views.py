5a8f2c7c368ad9860b9439c070242f76a8350f07
Filippe <revigat@gmail.com>
2016-08-18 02:26:47 -0300

    Ajuste do json

--- breadtimesite/views.py
+++ breadtimesite/views.py
@@ -1,14 +1,11 @@
 # -*- coding: UTF-8 -*-
-from django.http import JsonResponse
 from django.shortcuts import HttpResponse
-from django.core import serializers
 from django.shortcuts import render_to_response
 from breadtimesite.models import *
 from breadtimesite.models import Token
 from django.views.decorators.csrf import csrf_exempt
-from django.core.serializers import json
-from django.core.serializers.python import Serializer
-
+from breadtimesite.serializers import PostSerializer
+from rest_framework.renderers import JSONRenderer
 # Create your views here.
 
 
@@ -18,20 +15,12 @@ def index(request):
 
 
 def exportar_feed(request):
-    # Habilita os campos legiveis para aparecer no json
-    # https://docs.djangoproject.com/ja/1.9/topics/db/queries/
-
-    class MySerialiser(Serializer):
-        def end_object(self, obj):
-            self._current['id'] = obj._get_pk_val()
-            self.objects.append(self._current)
-
- # Views.py
-    serializer = MySerialiser()
-
-    json = serializer.serialize(Post.objects.filter(status=True),
-        indent=3, use_natural_foreign_keys=True, use_natural_primary_keys=True)
-    return HttpResponse(json, content_type="application/json; charset=utf-8")
+    # Recupera os dados
+    posts = Post.objects.all()
+    # Passa da dados para a classe que serializa e limpa
+    serializer = PostSerializer(posts, many=True)
+    json_feed = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_feed)
 
 
 @csrf_exempt
